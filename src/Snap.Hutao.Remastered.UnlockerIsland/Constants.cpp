685998c97d9f978d0d57d078e611b482467441a4
nahida-mouo <nahida-mouo@outlook.com>
2026-07-25 10:33:06 +0800

    将剑斗木桩添加到千星奇域白名单

--- src/Snap.Hutao.Remastered.UnlockerIsland/Constants.cpp
+++ src/Snap.Hutao.Remastered.UnlockerIsland/Constants.cpp
@@ -18,6 +18,7 @@ std::list<std::wstring> BeyondWhiteList
 	L"45694132064",
 	L"24339714898",
 	L"41376171429",
+	L"15605228819", // 剑斗木桩
 };
 
 const char* PAIMON_PATH = "/EntityRoot/OtherGadgetRoot/NPC_Guide_Paimon(Clone)";
